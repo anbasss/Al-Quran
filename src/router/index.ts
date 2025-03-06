8f856e43a5083de6895cc2fc0a555ac59b3f10e5
Anbas <andisulo434@gmail.com>
2025-03-06 20:29:09 +0800

    fix bugs to deploy

---
+++ src/router/index.ts
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import type { RouteRecordRaw } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+  },
+  // Add your other routes here
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+})
+
+export default router
